0c9f12b87bdf397c45af47f372dc48b9ff198bde
Karthik Palanisamy <pkarthikbit@gmail.com>
2020-12-04 09:22:57 +0530

    pin16 blink code

--- main/main.c
+++ main/main.c
@@ -1,6 +1,6 @@
-/* Hello World Example
+/* nanoDRONE 
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   This Prototype code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
@@ -9,30 +9,37 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
 
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+
+static const char *TAG = "main";
+
+#define GPIO_OUTPUT_IO_1    16
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_1))
 
 void app_main()
 {
-    printf("Hello world!\n");
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO15/16
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
+    int cnt = 0;
 
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    while (1) {
+        ESP_LOGI(TAG, "cnt: %d\n", cnt++);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
