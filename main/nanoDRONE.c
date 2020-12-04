3b5492de1be51d9f1386b06f6a4803738d659e4c
Karthik Palanisamy <pkarthikbit@gmail.com>
2020-12-04 09:47:04 +0530

    compiler error corrected

--- main/nanoDRONE.c
+++ main/nanoDRONE.c
@@ -6,8 +6,8 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <nanoDRONE_inf.h>
-#include <nanoDRONE_priv.h>
+#include "nanoDRONE_inf.h"
+#include "nanoDRONE_priv.h"
 
 static const char *TAG = "main";
 
