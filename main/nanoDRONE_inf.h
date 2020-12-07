8d73769b978e3572aa9c651da5b6700e705264a2
Karthik Palanisamy <pkarthikbit@gmail.com>
2020-12-07 18:49:38 +0530

    header file organised

--- main/nanoDRONE_inf.h
+++ main/nanoDRONE_inf.h
@@ -6,23 +6,25 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-
-#include <stdio.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-
 #include "driver/gpio.h"
 
+#include "esp_log.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
-#include "nvs_flash.h"
-#include <esp_http_server.h>
+#include "esp_http_server.h"
 #include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include "lwip/ip_addr.h"
 
+#include "nvs_flash.h"
+
+#include <stdio.h>
+#include <string.h>
 #include <sys/param.h>
+
